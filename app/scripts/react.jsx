a77588628466e9e75a473c5048e0ac2c4d0fab8c
Revath S Kumar <rsk@revathskumar.com>
2014-04-21 21:50:13 +0530

    A react example to update text from input

--- app/scripts/react.jsx
+++ app/scripts/react.jsx
@@ -10,3 +10,23 @@ var HelloMessage = React.createClass({
 
 React.renderComponent(<HelloMessage name="Revath" />, document.querySelector('.jumbotron'));
 
+
+var UpdateText = React.createClass({
+  getInitialState: function(){
+    return {name: ''}
+  },
+  change: function(e){
+    this.setState({name: e.target.value})
+  },
+  render: function(){
+    return (
+      <div>
+        <input type="text" name="name" onChange={this.change} />
+        <h1> Hello {this.state.name}!!!</h1>
+      </div>
+    )
+  }
+});
+
+React.renderComponent(<UpdateText/>, document.querySelector('.change-text'));
+
